0ca9d657b0d18f59014347a90bfe30d8e207c68d
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2018-05-10 21:53:02 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import reduxThunk from 'redux-thunk';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
+import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+ReactDOM.render(<Provider store={store} >
+    <App />
+</Provider>
+    ,
+    document.getElementById('root'));
 registerServiceWorker();
